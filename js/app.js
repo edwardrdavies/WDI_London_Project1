b85489d4c1a1050010b7937149e50f84c5d0eff0
edwardrdavies <edward_r_davies@hotmail.co.uk>
2016-10-07 14:53:13 +0100

    Select color of clicked squares

--- js/app.js
+++ js/app.js
@@ -13,7 +13,13 @@ var colorDisplay = document.getElementById("colorDisplay");
 
 colorDisplay.textContent = pickedColor;
 
-//add initial colors to squares 
 for(var i = 0; i < squares.length; i++) {
   squares[i].style.background = colors[i];
+//add initial colors to squares
+  squares[i].addEventListener("click", function(){
+    //grab color of clicked squares
+    var clickedColor = this.style.background;
+    //compare color to PickedColor
+  });
+
 }
