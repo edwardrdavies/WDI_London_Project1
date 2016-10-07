4451ea3a60ff2692ce1d89d77aea89017aea42bb
edwardrdavies <edward_r_davies@hotmail.co.uk>
2016-10-07 14:45:07 +0100

    Added initial colors to squares

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Set color varibales
 var colors = [
   "rgb(255, 0, 0)",
   "rgb(255, 255, 0)",
@@ -6,9 +7,13 @@ var colors = [
   "rgb(0, 0, 255)",
   "rgb(255, 0, 255)"
 ]
-
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
 
+colorDisplay.textContent = pickedColor;
+
+//add initial colors to squares 
 for(var i = 0; i < squares.length; i++) {
   squares[i].style.background = colors[i];
 }
