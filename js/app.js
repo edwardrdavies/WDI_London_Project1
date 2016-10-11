0a2d0c02924fd806f9c78fbd85de4278ba955b1d
edwardrdavies <edward_r_davies@hotmail.co.uk>
2016-10-11 11:12:40 +0100

    Added instructions modal

--- js/app.js
+++ js/app.js
@@ -108,7 +108,7 @@ for(var i = 0; i < squares.length; i++) {
     if(clickedColor === pickedColor) {
     //Step 5:4: If correct color is picked, strip to display "Correct"
       messageDisplay.textContent = "Correct";
-    //When correct add score to scoreboard 
+    //When correct add score to scoreboard
       score = maxPoints + score;
       scoreDisplay.textContent = score;
       resetButton.textContent= "Play Again?"
@@ -156,4 +156,30 @@ function randomColor() {
     //pick a "blue" from 0 -255
     var b = Math.floor(Math.random() * 256);
     return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
